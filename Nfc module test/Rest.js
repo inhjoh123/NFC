8f9f0b6de5edd61af3bee00bbcea945ac97f5c75
LeeKYHH <65584404+LeeKYHH@users.noreply.github.com>
2020-06-15 11:13:21 +0900

    Update Rest.js

--- Nfc module test/Rest.js
+++ Nfc module test/Rest.js
@@ -1,26 +1,26 @@
 import React , {Component} from "react";
 import {View, Text, StyleSheet,Button} from "react-native";
-import Axios from "axios";
+import Axios from "axios"; //http 요청을 위해 필요한 모듈
 
 export default class App extends Component {
   state = {};
 
-  constructor(props){
+  constructor(props){ //생성자 메서드
     super(props);
     this.setState({
       output : null,
     });
   }
 
-  _onPress = () => {
+  _onPress = () => { //
     Axios.get('https://jsonplaceholder.typicode.com/todos/1')//url로 get방식으로 요청
     .then(
         response => {//성공시
         console.log("response : ",response["config"]["validateStatus"]);
-        this.setState({output:response});
+        this.setState({output:response}); //get요청의 결과로 받은 데이터를 state에 저장.
       }
     )
-    .catch(
+    .catch( // 실패시
       error =>{
         console.log("error : ",error);
       }
